a995423e3f200158b7d3351adba4ce08e9793abc
Marco Túlio <marcopbreyner@gmail.com>
2025-03-12 20:54:28 -0300

    Create backend
    Conexão com o MySQL, feita API com 4 rotas pra CRUD e adicionado arquivo .gitignore

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'agenda',
+});
+
+app.post('/contatos', (req, res) => {
+    const { nome, telefone, email } = req.body;
+    connection.query('CALL pAdicionarContato(?, ?, ?)', [nome, telefone, email], (err, result) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.send({ message: 'Contato adicionado' });
+    });
+});
+
+app.get('/contatos', (req, res) => {
+    connection.query('CALL pListarContato()', (err, result) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.json(result[0]);
+    });
+});
+
+app.put('/contatos/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, telefone, email } = req.body;
+    connection.query(`CALL pAtualizarContato(${id, nome, telefone, email})`, (err, result) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.send({ message: 'Contato atualizado com sucesso!' });
+    });
+    console.log("Atualizando contato:", id, nome, telefone, email);
+});
+
+app.delete('/contatos/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query(`CALL pDeletarContato(${id})`, (err, result) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.send({ message: 'Contato deletado com sucesso!' });
+    });
+});
+
+app.listen(3000, () => {console.log("Servidor rodando na porta 3000")});
